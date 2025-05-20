de06ea7333b2035b4b618c0775ec5b6efba6a9da
Faizan707 <faizanbutt707@gmail.com>
2025-05-20 21:53:25 +0500

    make frontend navbar and also create routes

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -21,6 +21,10 @@ const userSchema = new mongoose.Schema({
     unique: true,
     sparse: true,
   },
+  avatar: {
+    type: String,
+    default:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Ffree-vector%2Fuser-icon&psig=AOvVaw3eu9j9H9BcvHz-gCcNzpiv&ust=1747784110374000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPDe66nZsI0DFQAAAAAdAAAAABAE'
+  },
 });
 
 userSchema.pre('save', async function (next) {
---
+++ frontend/src/app/feeds/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '@/components/Navbar';
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <main className="mt-4">
+        {children}
+      </main>
+    </div>
+  );
+}
